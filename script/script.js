a3ab8938e16fb5216aab75fd2c13be252e452ed1
Aida1905 <miss.wowhey@gmail.com>
2021-09-18 15:20:07 +0600

    initial

---
+++ script/script.js
@@ -0,0 +1,51 @@
+// let a ="welcome";
+// let b = 5;
+// function word(y, z) {
+   // return y+z
+// }
+// word ("welcome", 10);
+
+// console.log(word ("welcome", 10));
+
+
+// const num= "значение";
+// console.log(num);
+
+
+// const a = 15;
+// const b = 10;
+// const c = 5;
+
+// console.log(a + b + c)
+
+// const a = 15;
+// const b = 10;
+// const c = 5;
+
+// function sum(a, b, c) {
+    // return (a + b + c)
+// }
+
+//console.log(sum (15; 10; 5))
+
+// const a = 100;
+
+// function sum(a) {
+// return(a * 2)
+// }
+
+//console.log(sum(a))
+
+// const a = 67;
+// function del(a) {
+// return (a % 7)
+// }
+
+// console.log(del(67))
+
+// const a = 17;
+// const b = 10;
+// const c = a - b;
+// const d = c;
+// const result = c + d;
+// console.log(result)
